30ba9c97fd4e4fa98155387a72f1103fb886f022
Rein F <public@reinfernhout.xyz>
2019-02-20 20:02:23 +0100

    Create uploader

---
+++ create-uploaders.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const cf = require(path.join(__dirname, "/config/config.js"));
+const chalk = require("chalk");
+
+const image = {
+    Name: "Eren image upload",
+    DestinationType: "ImageUploader",
+    RequestURL: `${cf.protocol}://${cf.domain}/s/image`,
+    FileFormName: "image",
+    Arguments: {
+      key: "access-key-here"
+    },
+    URL: "$json:url$",
+    DeletionURL: "$json:delete$"
+}
+
+const text = {
+    Name: "Eren text upload",
+    DestinationType: "URLShortener",
+    RequestURL: `${cf.protocol}://${cf.domain}/s/text`,
+    Arguments: {
+        text: "$inpu$",
+        key: "access-key-here"
+    },
+    URL: "$json:url$",
+    DeletionURL: "$json:delete$"
+}
+
+const url = {
+    Name: "Eren url shorten",
+    DestinationType: "URLShortener",
+    RequestURL: `${cf.protocol}://${cf.domain}/s/url`,
+    Arguments: {
+        url: "$inpu$",
+        key: "access-key-here"
+    },
+    URL: "$json:url$",
+    DeletionURL: "$json:delete$"
+}
+
+process.stdout.write(`${chalk.green("Eren_image_upload.sxcu")}:\n${JSON.stringify(image)}\n`);
+process.stdout.write(`${chalk.green("Eren_text_upload.sxcu")}:\n${JSON.stringify(text)}\n`);
+process.stdout.write(`${chalk.green("Eren_url_shorten.sxcu")}:\n${JSON.stringify(url)}\n`);
