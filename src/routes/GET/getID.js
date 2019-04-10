34b01b72dcbc69e35d10cbd550feb03803b21cf7
Rein F <public@reinfernhout.xyz>
2019-04-10 10:58:11 +0200

    favicons now handled by getID.js directly

--- src/routes/GET/getID.js
+++ src/routes/GET/getID.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const {REQ, RES} = require(path.join(__dirname, "../../util/logger"));
+const replacement_image = require(path.join(__dirname, "replacement_image"));
 
 module.exports = enmap => ( req, res ) => {
 
@@ -11,8 +12,12 @@ module.exports = enmap => ( req, res ) => {
 
             //When there is a . in the requested ID we assume the client attempted to retrieve an image
             if (req.params.id.includes(".")) {
-                RES({code: 404, msg:"Image not found"});
-                return replacement_image( req, res );
+                if (req.url == "/favicon.ico")
+                    return res.sendFile(path.join(__dirname, `../../../default_images/favicon.ico`))
+                else {
+                    RES({code: 404, msg:"Image not found"});
+                    return replacement_image( req, res );
+                }
             } else {
                 RES({code: 404, msg:"ID not found"});
                 return res.status(404).render("index", {message: "ID not found"});
--- src/routes/GET/replacement_image.js
+++ src/routes/GET/replacement_image.js
@@ -1,9 +1,6 @@
 const path = require("path");
 
 module.exports = (req,res) => {
-    if (req.url == "/favicon.ico")
-        return res.sendFile(path.join(__dirname, `../../../default_images/favicon.ico`))
-
     let i = Math.floor(Math.random() * 3)+1;
     res.status(404).sendFile(path.join(__dirname, `../../../default_images/image_not_found${i}.png`))
 }
