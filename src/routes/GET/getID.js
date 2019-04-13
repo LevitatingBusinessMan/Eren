63a309f978514c6b4885e803fba0ed508293b16d
Rein F <public@reinfernhout.xyz>
2019-04-13 17:33:26 +0200

    fixed favicons for subdomainless domains

--- src/routes/GET/getID.js
+++ src/routes/GET/getID.js
@@ -4,20 +4,21 @@ const replacement_image = require(path.join(__dirname, "replacement_image"));
 const r = require(path.join(__dirname, "../../util/db"));
 
 module.exports = async ( req, res ) => {
+
+    if (req.url == "/favicon.ico")
+        return res.sendFile(path.join(__dirname, `../../../default_images/favicon.ico`))
+
     //split id of any file extensions
     const id = req.params.id.split(".")[0];
+    
     if (req.subdomains.length || req.path.match(/(\/i\/[a-z,A-Z]*)(\.[a-z,A-Z]*)?/)) { //matches: /i/id and /i/id.ext
         let obj = await r.table("units").get(id).run();
         if (!obj) {
 
             //When there is a . in the requested ID we assume the client attempted to retrieve an image
             if (req.params.id.includes(".")) {
-                if (req.url == "/favicon.ico")
-                    return res.sendFile(path.join(__dirname, `../../../default_images/favicon.ico`))
-                else {
-                    RES({code: 404, msg:"Image not found"});
-                    return replacement_image( req, res );
-                }
+                RES({code: 404, msg:"Image not found"});
+                return replacement_image( req, res );
             } else {
                 RES({code: 404, msg:"ID not found"});
                 return res.status(404).render("index", {message: "ID not found"});
