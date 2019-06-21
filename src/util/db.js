5a601745da7ecf82c5f8869c09db9aa82ed9835f
Rein F <public@reinfernhout.xyz>
2019-06-21 16:14:55 +0200

    Wait for DB to be made before making tables

--- src/util/db.js
+++ src/util/db.js
@@ -4,33 +4,36 @@ const bcrypt = require("bcrypt");
 const r = require("rethinkdbdash")(config.Rethink);
 
 //Check if DB exists
-r.dbList().contains(config.Rethink.db).do(exists => r.branch(exists, {dbs_created:0}, r.dbCreate(config.Rethink.db))).run();
+r.dbList().contains(config.Rethink.db).do(exists => r.branch(exists, {dbs_created:0}, r.dbCreate(config.Rethink.db))).run().then(() => {
 
-//Check for Units table
-r.tableList().contains("units").do(exists => r.branch(exists, {dbs_created:0}, r.tableCreate("units"))).run()
-.then(() => r.table("units").indexList().contains("del_key").do(exists => r.branch(exists, {created:0}, r.table("units").indexCreate("del_key"))).run())
+    //Check for Units table
+    r.tableList().contains("units").do(exists => r.branch(exists, {dbs_created:0}, r.tableCreate("units"))).run()
+    .then(() => r.table("units").indexList().contains("del_key").do(exists => r.branch(exists, {created:0}, r.table("units").indexCreate("del_key"))).run())
 
-//Check for tokens table
-r.tableList().contains("tokens").do(exists => r.branch(exists, {dbs_created:0}, r.tableCreate("tokens", {primaryKey: "token"}))).run();
+    //Check for tokens table
+    r.tableList().contains("tokens").do(exists => r.branch(exists, {dbs_created:0}, r.tableCreate("tokens", {primaryKey: "token"}))).run();
 
-//Check for users table
-r.tableList().contains("users").do(exists => r.branch(exists, {dbs_created:0}, r.tableCreate("users", {primaryKey: "email"}))).run()
-.then(() => r.table("users").indexList().contains("token").do(exists => r.branch(exists, {created:0}, r.table("users").indexCreate("token"))).run())
+    //Check for users table
+    r.tableList().contains("users").do(exists => r.branch(exists, {dbs_created:0}, r.tableCreate("users", {primaryKey: "email"}))).run()
+    .then(() => r.table("users").indexList().contains("token").do(exists => r.branch(exists, {created:0}, r.table("users").indexCreate("token"))).run())
 
-//Check if admin user was made
-.then(() => r.table("users").getAll("admin", index="token").count().eq(1).run().then(exists => {
-    if (!exists)
-        bcrypt.hash(config.admin.pass, 12, function(err, hash) {
-            if (err)
-                throw err;
-            r.table("users").insert({
-                email:config.admin.email,
-                password:hash,
-                signup_token:"admin",
-                sharex_tokens:[]
-            }).run()
-        })
-}))
+    //Check if admin user was made
+    .then(() => r.table("users").getAll("admin", index="token").count().eq(1).run().then(exists => {
+        if (!exists)
+            bcrypt.hash(config.admin.pass, 12, function(err, hash) {
+                if (err)
+                    throw err;
+                r.table("users").insert({
+                    email:config.admin.email,
+                    password:hash,
+                    signup_token:"admin",
+                    sharex_tokens:[]
+                }).run()
+            })
+    }))
+
+})
+
 
 /*
 User:
